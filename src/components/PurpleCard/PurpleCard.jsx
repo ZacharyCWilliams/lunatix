2496affe3ae4f49b029a705a60cd3df76e5ed09d
zach <zachcwilliams@icloud.com>
2020-10-16 15:15:21 -0700

    updates to purple card section

--- src/components/PurpleCard/PurpleCard.jsx
+++ src/components/PurpleCard/PurpleCard.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import "./PurpleCard.scss";
-import ViewAllBtn from "../../assets/blank-button.png"
 import CardMaroon from "./CardMaroon";
 
 export default function PurpleCard() {
   return (
     <section className='purple-container'>
       <div className="purple-buttons">
-        <h2 className="trending-h2">Trending</h2>
-        {/* need to add title to button  */}
-        <img src={ViewAllBtn} alt="View All"/>
+        <h2 className="trending-h2">TRENDING</h2>
+        <button className="view-all-btn">
+          View All
+        </button>
       </div>
       <div className="purple-cards">
         <CardMaroon title="EVENT 1" color="#690d3e" />
